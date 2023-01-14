50a9abe0f0a970a23009275e887b372549feb156
Denis Borisov <defdden@mail.ru>
2023-01-14 16:01:13 +0300

    Improve credentials args

--- main.py
+++ main.py
@@ -13,60 +13,66 @@ from Classes.Functions import Authorize, Logoff, SetSessionHeaders
 from Classes.DownloadBook import downloadBook
 
 if __name__ == "__main__":
+    appDir = path.dirname(argv[0])
     parser = argparse.ArgumentParser()
-    inputGroup = parser.add_mutually_exclusive_group(required=True)
-    parser.add_argument(
+    urlGroup = parser.add_mutually_exclusive_group(required=True)
+    credentialsGroup = parser.add_mutually_exclusive_group(required=True)
+    lpGroup = credentialsGroup.add_argument_group()
+    lpGroup.add_argument(
         '-l',
         '--login',
         type=str,
         help='Email to login'
     )
-    parser.add_argument(
+    lpGroup.add_argument(
         '-p',
         '--password',
         type=str,
         help='Password to login'
     )
-    parser.add_argument(
+    credentialsGroup.add_argument(
         '-c',
         '--credentials',
         type=str,
+        default=(appDir + '/PrivateConfig'),
         help='Path to the folder with email.txt, password.txt'
     )
     parser.add_argument(
         '-o',
         '--output',
         type=str,
-        help='Path to the output folder',
+        help='Path to the output folder'
     )
     parser.add_argument(
         '-v',
         '--verbose',
         default=False,
-        action="store_true"
+        action="store_true",
+        help="Show book chapters"
     )
-    inputGroup.add_argument(
+    urlGroup.add_argument(
         '-u',
         '--url-list',
         nargs='+',
-        help="List of urls to download",
+        help="List of urls to download"
     )
-    inputGroup.add_argument(
+    urlGroup.add_argument(
         '-i',
         '--input-file',
-        help="Input file with urls to download",
+        help="Input file with urls to download"
     )
     args = parser.parse_args()
 
-    fileDir = path.dirname(argv[0])
-    outputDir = fileDir + "/Output" if not args.output else args.output
+    
+    outputDir = appDir + "/Output" if not args.output else args.output
     makedirs(outputDir, exist_ok=True)
 
     client = Client()
     client._timeout = Timeout(3)
     client.base_url = Pages.main
     SetSessionHeaders(client)
-    credentialsFolder = fileDir + "/PrivateConfig" if not args.credentials else args.credentials
+
+    credentialsFolder = args.credentials
     emailPath = credentialsFolder + "/email.txt"
     passwordPath = credentialsFolder + "/password.txt"
     authorized = False
@@ -74,13 +80,13 @@ if __name__ == "__main__":
         email = args.login
         password = args.password
         authorized = Authorize(client, email, password)
-    elif (path.exists(emailPath)
-        and path.exists(passwordPath)):
+    elif (path.exists(emailPath) and path.exists(passwordPath)):
         with open(emailPath) as f:
             email = f.readline()
         with open(passwordPath) as f:
             password = f.readline()
-        authorized = Authorize(client, email, password)
+        if (email != '' and password != ''):
+            authorized = Authorize(client, email, password)
 
     if authorized:
         print(f"Authorized as {email}")
@@ -91,12 +97,15 @@ if __name__ == "__main__":
     t = time()
     if args.url_list:
         for url in args.url_list:
-            downloadBook(
-                client,
-                authorized,
-                sub(url_regexp, '', url),
-                outputDir,
-                args.verbose)
+            try:
+                downloadBook(
+                    client,
+                    authorized,
+                    sub(url_regexp, '', url),
+                    outputDir,
+                    args.verbose)
+            except Exception:
+                    print(f"Fail to download {url}")
     elif (args.input_file):
         with open(args.input_file, 'r') as f:
             for line in f:
